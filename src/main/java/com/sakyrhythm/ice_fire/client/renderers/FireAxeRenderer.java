8345d33cc3d4c5f96f0e0a647253fc91b6182d47
crystalneko <minecrafthetun@outlook.com>
2024-08-18 22:38:18 +0800

    添加实体

---
+++ src/main/java/com/sakyrhythm/ice_fire/client/renderers/FireAxeRenderer.java
@@ -0,0 +1,19 @@
+package com.sakyrhythm.ice_fire.client.renderers;
+
+import com.sakyrhythm.ice_fire.item.ActivatedFireTool.FireAxeEntity;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.util.Identifier;
+
+public class FireAxeRenderer extends EntityRenderer<FireAxeEntity> {
+    public FireAxeRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public Identifier getTexture(FireAxeEntity entity) {
+        //TODO 完成它
+        return;
+    }
+
+}
